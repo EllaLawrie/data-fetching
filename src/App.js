8616ae6e727578db59d82f749e35e030f1d4fac5
EllaLawrie <lawrieella002@gmail.com>
2023-08-16 20:27:15 +0200

    data-fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        setUsers(response.data)
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gray-100 min-h-screen flex items-center justify-center'>
+      <div className='bg-gray-300 shadow-md rounded-md p-4 w-full max-w-md'>
+        <h1 className='text-4xl font-bold mb-6 flex items-center justify-center'>
+          Users List
+        </h1>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id} className='mb-2'>
+              <strong>{user.name}</strong> ({user.email})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
